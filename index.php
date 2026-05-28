5d03ebb051110315d50352222b30fe6fe53f8f31
KLEITON SILVA FERREIRA <25606@pmc.local>
2026-05-28 09:53:52 -0300

    .htaccess e index.php realocado na raiz para retirada do '/public' na url

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 
-/* This will give an error. Note the output
- * above, which is before the header() call */
-header('Location:https://estrategiadigital.caraguatatuba.sp.gov.br/public');
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require __DIR__.'/vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
